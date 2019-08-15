8f184529e8643275ee0f1f073b3ea3215d1173c0
shaheensms <shaheensms111@gmail.com>
2019-08-15 23:42:35 +0600

    Login, SignUp, SignInRegister layout done

---
+++ app/src/main/java/com/metacoders/hurrydriver/SignIn_Register.java
@@ -0,0 +1,14 @@
+package com.metacoders.hurrydriver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignIn_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in__register);
+    }
+}
