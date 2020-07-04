0f1b35c1f6ca5804e2c71a9ee3bda9ca44cf26c1
rahat14 <rahatshovo6666@gmail.com>
2020-07-05 03:48:50 +0600

    a

--- app/src/main/java/com/metacoders/hurrydriver/Activity/MainActivity.java
+++ app/src/main/java/com/metacoders/hurrydriver/Activity/MainActivity.java
@@ -23,10 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
-
-
-
         tabLayout= (TabLayout)findViewById(R.id.tabLayout) ;
         //  appBarLayout = findViewById(R.id.appbarId) ;
         viewPager =findViewById(R.id.viewPager) ;
